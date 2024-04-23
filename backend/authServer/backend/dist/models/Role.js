7536881805c6e84f279da81a7fe154563e040683
Olga_Belaya <protenot@yandex.ru>
2024-04-23 21:36:02 +0300

    clear master

--- backend/authServer/backend/dist/models/Role.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const Role = new mongoose_1.Schema({
-    value: { type: String, unique: true, default: "USER" },
-});
-exports.default = (0, mongoose_1.model)("Role", Role);
